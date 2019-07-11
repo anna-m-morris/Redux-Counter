ea57ae9f7ee4df88c38128ae92d3e20a1d30092b
clay-most <46730958+clay-most@users.noreply.github.com>
2019-07-10 18:47:30 -0600

    remove console logs

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -14,9 +14,6 @@ class Counter extends Component {
     };
 
     render() {
-        console.log(this.props.decrement)
-        console.log(this.props.increment)
-        console.log(this.props.count)
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
         // should decrement or increment accordingly
